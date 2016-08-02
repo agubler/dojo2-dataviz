27297e7e489a7d00e7d239ddb9a9276f326d5685
Mark Wubben <mark@novemberborn.net>
2016-08-02 11:00:10 +0100

    fixup! fixup! fixup! fixup! fixup! fixup! fixup! wip! Support negative values

--- examples/negatives.ts
+++ examples/negatives.ts
@@ -2,6 +2,7 @@ import projector from 'dojo-widgets/projector';
 
 import max from 'src/data/max';
 import createColumnChart, { ColumnChartFactory } from 'src/render/createColumnChart';
+import createGroupedColumnChart, { GroupedColumnChartFactory } from 'src/render/createGroupedColumnChart';
 
 const basic = (<ColumnChartFactory<number>> createColumnChart)({
 	bottomAxis: {
@@ -95,13 +96,40 @@ const allNegativeDomain = (<ColumnChartFactory<number>> createColumnChart)({
 	xInset: 30
 });
 
+const group = (<GroupedColumnChartFactory<string, [string, number]>> createGroupedColumnChart)({
+	bottomAxis: {
+		inputs: {
+			labelSelector({ input }) { return input; }
+		},
+		labels: { anchor: 'middle', textAnchor: 'end', rotation: -45, offset: -5 },
+		ticks: { anchor: 'end', length: 5, zeroth: true }
+	},
+	columnHeight: 150,
+	columnSpacing: 3,
+	columnWidth: 10,
+	divisorOperator: max,
+	height: 200,
+	groupSelector(input: [string, number]) { return input[0]; },
+	inputSeries: [['foo', -5], ['foo', -5], ['bar', 5], ['bar', 10]],
+	leftAxis: {
+		labels: { anchor: 'end' },
+		range: { stepSize: 5 },
+		ticks: { length: 5, zeroth: true }
+	},
+	valueSelector(input: [string, number]) { return input[1]; },
+	width: 100,
+	xInset: 30
+});
+
 (<any> window).basic = basic;
 (<any> window).domain = domain;
 (<any> window).allNegative = allNegative;
 (<any> window).allNegativeDomain = allNegativeDomain;
+(<any> window).group = group;
 
 projector.append([
 	basic, domain,
-	allNegative, allNegativeDomain
+	allNegative, allNegativeDomain,
+	group
 ]);
 projector.attach();
